66e7c73acf7ca5fa81fc167d2e4ffe106e4efa5e
TheOneKevin <kevindai02@outlook.com>
2021-01-05 14:18:56 -0500

    Updated settings and docs

--- enc.js
+++ enc.js
@@ -21,22 +21,46 @@
 const fs = require('fs');
 
 const blinky = [
-    '2400FF00', // MOV r0 <- 0xFF00
-    '24100000', // MOV r1 <- 0x0000
-    '34100084', // STR [r0+0x84] <- r1
-    '241001FF', // MOV r1 <- 0x01FF
-    '34100080', // STR [r0+0x80] <- r1
-    '2420FFFF', // MOV r2 <- 0xFFFF
-    '64220001', // SUB r2 <- r2 - 1
+    '2400FF00', // MOV r0 <- 0xFF00 ; AICT base address
+    '241016E3', // MOV r1 <- 0x16E3 ; Timer match ~ 1 Hz
+    '2420002D', // MOV r2 <- 0x2D   ; Timer config = 01 01 101 (toggle normal 1024)
+    '341000A4', // STR [r0+0xA4] <- r1
+    '342000A0', // STR [r0+0xA0] <- r2
+    '50000000', // B<0000> 0
+];
+
+const uart = [
+    '2400FF00', // MOV r0 <- 0xFF00 ; AICT base
+    '24100068', // MOV r1 <- 0x68   ; 12Mhz/115200Hz
+    '341000BC', // STR [r0+BC] <- r1
+    '24100FFF', // MOV r1 <- 0x0FFF ; Set GPIO mode out
+    '34100080', // STR [r0+80] <- r1
+    '24100059', // MOV r1 <- [data-3]
+    // Loop
+    '14210000', // LDR r2 <- [r1]
+    '842200FF', // AND r2 <- r2 & 0xFF
     '6C220000', // CMP r2, 0
-    '52FFFFF8', // B<0010> -8
-    '24100001', // MOV r1 <- 0x0001
-    '34100084', // STR [r0+0x84] <- r1
-    '2420FFFF', // MOV r2 <- 0xFFFF
-    '64220001', // SUB r2 <- r2 - 1
-    '6C220000', // CMP r2, 0
-    '52FFFFF8', // B<0010> -8
-    '24F00000', // MOV PC, 0
+    '51000010', // B<0001> [end]
+    '70000018', // B<0000>L [write]
+    '44110001', // ADD r1 <- r1 + 1
+    '50FFFFE8', // B<0000> [loop]
+    // End
+    '24200800', // MOV r2 <- 0x0800 ; Set green LED
+    '34200084', // STR [r0+84] <- r2
+    '50000000', // B<0000> 0
+    // Write subroutine
+    '24300001', // MOV r3 <- 1  ; Do TX write (badness 1000)
+    '342000B0', // STR [r0+B0] <- r2
+    '343000B8', // STR [r0+B8] <- r3
+    '144000B8', // LDR r4 <- [r0+B8]
+    '8C040020', // TST r4, 0x20 ; Test TX ready
+    '52FFFFF8', // B<0010> -8   ; Loop if not zero
+    '20F0E000', // MOV pc <- lr ; Return
+    // Data
+    '48656c6c',
+    '6f2c2077',
+    '6f726c64',
+    '21000000'
 ];
 
 // Outputs the 4 bram files
@@ -68,6 +92,8 @@ function encode(arr, sz) {
 
 ///////////////////////////////////////////////////////////////////////////////
 
+const default_sz = 1024;
+
 const readline = require('readline');
 const { exit } = require('process');
 const ql = readline.createInterface({
@@ -82,9 +108,9 @@ function prompt(str) {
     });
 }
 async function main() {
-    let bits = await prompt('32-bit words in ram (default 256): ');
+    let bits = await prompt(`32-bit words in ram (default ${default_sz}): `);
     bits = parseInt(bits);
-    bits = bits ? bits : 256;
+    bits = bits ? bits : default_sz;
     let prog = [];
     console.log('Enter program hex array:');
     for(i = 0; i < bits; i++) {
@@ -115,7 +141,10 @@ if(!arg1) {
     main();
 } else if(arg1 == 'blinky') {
     process.stdout.write('Encoding blinky... ');
-    encode(blinky, 256);
+    encode(blinky, default_sz);
+} else if(arg1 == 'uart') {
+    process.stdout.write('Encoding uart... ');
+    encode(uart, default_sz);
 } else if(arg1 == 'file') {
     let file = process.argv[3];
     let bits = process.argv[4];
@@ -127,15 +156,15 @@ if(!arg1) {
         //console.log(prog);
         bits = parseInt(bits);
         if(!bits) {
-            console.warn(`\u001b[33m%s\u001b[0m`, 'Invalid word length. Defaulting to 256.');
-            bits = 256;
+            console.warn(`Invalid word length. Defaulting to ${default_sz}.`);
+            bits = default_sz;
         }
         encode(prog, bits);
     } else {
         console.error('File not found!');
         exit(-1);
     }
-    encode(blinky, 256);
+    encode(blinky, default_sz);
 } else {
     console.error(`Unknown argument: ${arg1}.`);
     console.log('Usage: node ./enc.js [ blinky | file ] [filename?] [words?]');
